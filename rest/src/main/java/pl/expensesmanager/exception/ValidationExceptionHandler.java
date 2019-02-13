f19c7c3ee84641ed3768edd2a21f01b00fb904a6
Mateusz Mucha <mateusz.marek.mucha@gmail.com>
2019-02-13 23:02:23 +0100

    Add missing annotations to controller advice

--- rest/src/main/java/pl/expensesmanager/exception/ValidationExceptionHandler.java
+++ rest/src/main/java/pl/expensesmanager/exception/ValidationExceptionHandler.java
@@ -3,6 +3,7 @@ package pl.expensesmanager.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import pl.expensesmanager.exception.validation.ValidateDateException;
 import pl.expensesmanager.exception.validation.ValidateNumberException;
@@ -12,24 +13,28 @@ import pl.expensesmanager.exception.validation.ValidateTextException;
 @ControllerAdvice
 public class ValidationExceptionHandler {
 	
+	@ResponseBody
 	@ResponseStatus(HttpStatus.CONFLICT)
 	@ExceptionHandler(ValidateTextException.class)
 	public ExceptionMessage handleTextException(ValidateTextException exception) {
 		return exceptionMessage(exception);
 	}
 	
+	@ResponseBody
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler(ValidateObjectException.class)
 	public ExceptionMessage handleTextException(ValidateObjectException exception) {
 		return exceptionMessage(exception);
 	}
 	
+	@ResponseBody
 	@ResponseStatus(HttpStatus.CONFLICT)
 	@ExceptionHandler(ValidateNumberException.class)
 	public ExceptionMessage handleTextException(ValidateNumberException exception) {
 		return exceptionMessage(exception);
 	}
 	
+	@ResponseBody
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 	@ExceptionHandler(ValidateDateException.class)
 	public ExceptionMessage handleTextException(ValidateDateException exception) {
