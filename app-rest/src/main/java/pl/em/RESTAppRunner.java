64adf30364dee7085b4fb68db0c9cdfd6f6888a9
Mateusz Mucha <mateusz.marek.mucha@gmail.com>
2019-05-02 18:11:22 +0200

    Initialize basic working app

---
+++ app-rest/src/main/java/pl/em/RESTAppRunner.java
@@ -0,0 +1,12 @@
+package pl.em;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTAppRunner {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RESTAppRunner.class, args);
+	}
+	
+}
